90966c18315fceedb94ea5c443149b65048aa785
EnzoCglc <enzo.cougoulic@gmail.com>
2025-09-24 01:38:53 +0200

    Remake board with go template

--- controllers/game.go
+++ controllers/game.go
@@ -5,10 +5,22 @@ import (
 	"net/http"
 )
 
+// Create 2D matrix to represents the game board
+type GridPage struct {
+	Columns [][]int 
+}
 func SwitchPlay(w http.ResponseWriter, r *http.Request) {
+	//create 7 columns 
+	cols := make([][]int, 7)
+    for i := 0; i < 7; i++ {
+        cols[i] = make([]int, 6) // create 6 case empty
+    }
 	if r.Method == "POST" {
+
 		name := r.FormValue("play")
 		log.Println(name)
-		render(w, "gameBoard.html" , nil)
+
+		data := GridPage{Columns: cols}
+		render(w, "gameBoard.html" , data)
 	}
 }

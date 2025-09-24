0bd80a56e6e8c60f52e6d01874aa9f4152d81865
EnzoCglc <enzo.cougoulic@gmail.com>
2025-09-24 10:36:47 +0200

    Detect click at column

--- controllers/game.go
+++ controllers/game.go
@@ -7,20 +7,28 @@ import (
 
 // Create 2D matrix to represents the game board
 type GridPage struct {
-	Columns [][]int 
+	Columns [][]int
 }
-func SwitchPlay(w http.ResponseWriter, r *http.Request) {
-	//create 7 columns 
-	cols := make([][]int, 7)
-    for i := 0; i < 7; i++ {
-        cols[i] = make([]int, 6) // create 6 case empty
-    }
-	if r.Method == "POST" {
 
+func SwitchPlay(w http.ResponseWriter, r *http.Request) {
+	//create 7 columns
+	cols := make([][]int, 7)
+	for i := 0; i < 7; i++ {
+		cols[i] = make([]int, 6) // create 6 case empty
+	}
+	if r.Method == "POST" {
+		// Detect start game
 		name := r.FormValue("play")
 		log.Println(name)
+		//Detect clic in column
+		col := r.FormValue("col")
+		play(col)
 
 		data := GridPage{Columns: cols}
-		render(w, "gameBoard.html" , data)
+		render(w, "gameBoard.html", data)
 	}
+}
+
+func play(col string) {
+	log.Println("col", col, "has clicked")
 }

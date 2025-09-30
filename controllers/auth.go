19e51e51ea7b7cb473eafa335d54060a42061a06
ledgar <edgar.lasnier@ynov.com>
2025-09-30 10:47:22 +0200

    Change Branch Name

--- controllers/auth.go
+++ controllers/auth.go
@@ -6,11 +6,9 @@ import (
 )
 
 func LoginPage(w http.ResponseWriter, r *http.Request) {
-	// action := r.FormValue("play")
 	utils.Render(w, "loginPage.html", nil)
 }
 
 func RegisterPage(w http.ResponseWriter, r *http.Request) {
-	// action := r.FormValue("play")
 	utils.Render(w, "registerPage.html", nil)
 }

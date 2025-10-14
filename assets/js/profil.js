19542bdf760147c39e49c60e57197f9b59e27b07
Stormi <xstormi1910@gmail.com>
2025-10-14 12:24:40 +0200

    userdata

---
+++ assets/js/profil.js
@@ -0,0 +1,16 @@
+let userData = {
+    username: 'enzo',
+    elo: 1019,
+    victoires: 50,
+    defaites: 40,
+    historique: [
+        { date: '2025-10-14', adversaire: 'Player1', resultat: 'victoire', eloChange: +15 },
+        { date: '2025-10-13', adversaire: 'Player2', resultat: 'defaite', eloChange: -12 },
+        { date: '2025-10-13', adversaire: 'Player3', resultat: 'victoire', eloChange: +18 },
+        { date: '2025-10-12', adversaire: 'Player4', resultat: 'victoire', eloChange: +14 },
+        { date: '2025-10-11', adversaire: 'Player5', resultat: 'defaite', eloChange: -10 },
+        { date: '2025-10-10', adversaire: 'Player6', resultat: 'victoire', eloChange: +16 },
+        { date: '2025-10-09', adversaire: 'Player7', resultat: 'defaite', eloChange: -13 },
+        { date: '2025-10-08', adversaire: 'Player8', resultat: 'victoire', eloChange: +17 }
+    ]
+};

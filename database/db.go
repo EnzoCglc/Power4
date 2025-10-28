ead8bb952909701cf51b14e1e6a1c17828606933
EnzoCglc <enzo.cougoulic@gmail.com>
2025-10-28 10:27:39 +0100

    ✨ Create table users

---
+++ database/db.go
@@ -0,0 +1,43 @@
+package database
+
+import (
+	"database/sql"
+	"log"
+
+	_ "github.com/mattn/go-sqlite3" 
+)
+
+var DB *sql.DB
+const datafile string = "database/data.db" 
+
+func InitDB(){
+	var err error
+
+	DB,err = sql.Open("sqlite3", datafile)
+	if err != nil {
+		log.Println("Failed to connect Sqlite", err)
+	}
+
+	if err = DB.Ping(); err != nil {
+		log.Println("Failed to ping db ")
+	}
+
+	log.Println("Sqlite Connected")
+
+	createTable()
+}
+
+func createTable() {
+	query := `
+	CREATE TABLE IF NOT EXISTS users (
+		id INTEGER PRIMARY KEY AUTOINCREMENT,
+		username TEXT NOT NULL UNIQUE,
+		email TEXT,
+		created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+	);`
+
+	_ , err := DB.Exec(query)
+	if err != nil {
+		log.Println("Failed to create table", err)
+	}
+}
--- main.go
+++ main.go
@@ -3,17 +3,42 @@ package main
 import (
 	"log"
 	"net/http"
+	"power4/database"
 	"power4/routes"
 )
 
 func main() {
+	// Init Database
+	database.InitDB()
+	
 	// Setup routes
 	routes.SetupRoutes()
 
+	_, err := database.DB.Exec(`INSERT INTO users (username, email) VALUES (?, ?)`, "enzo", "enzo@example.com")
+	if err != nil {
+		log.Println("ERROR when insert data:", err)
+	}
+
+	table, err := database.DB.Query(`SELECT id, username, email FROM users`)
+	if err != nil {
+		log.Println("Error when listen table")
+	}
+
+	for table.Next() {
+		var id int
+		var username string
+		var email string
+		table.Scan(&id, &username, &email)
+		log.Printf("Valeur de l'id : %d  Valeur de l'username : %s Valeur de l'email : %s",id, username, email)
+	}
+
+	defer table.Close()
+	defer database.DB.Close()
+
 	// Start Server on localhost:8080
 	log.Print("Listening on localhost:8080...")
-	err := http.ListenAndServe(":8080", nil)
-	if err != nil {
-		log.Fatal(err)
+	error := http.ListenAndServe(":8080", nil)
+	if error != nil {
+		log.Fatal(error)
 	}
 }
